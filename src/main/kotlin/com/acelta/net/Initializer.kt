40e0bfe0a4c10b5e06282cd55ac6c07a6f015166
Thomas Nappo <technology@live.com>
2016-05-03 16:21:05 -0400

    Added `Initializer`

---
+++ src/main/kotlin/com/acelta/net/Initializer.kt
@@ -0,0 +1,14 @@
+package com.acelta.net
+
+import io.netty.channel.ChannelHandler
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.socket.SocketChannel
+
+@ChannelHandler.Sharable
+internal object Initializer : ChannelInitializer<SocketChannel>() {
+
+	override fun initChannel(ch: SocketChannel) {
+		ch.pipeline().addLast(Decoder(), Handler())
+	}
+
+}
