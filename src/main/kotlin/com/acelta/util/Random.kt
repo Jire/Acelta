f7493eb0eea2d56d07cef681e1db870cbaee9652
Thomas Nappo <technology@live.com>
2016-05-03 16:49:53 -0400

    `Random` is no longer thread-safe, but it is faster and more random

--- src/main/kotlin/com/acelta/util/Random.kt
+++ src/main/kotlin/com/acelta/util/Random.kt
@@ -1,20 +1,16 @@
 package com.acelta.util
 
-import java.util.concurrent.ThreadLocalRandom
+import java.security.SecureRandom
 
 object Random {
 
-	private var x = ThreadLocal.withInitial { ThreadLocalRandom.current().nextLong(Long.MAX_VALUE) }
+	private var x = SecureRandom().nextLong()
 
 	operator fun get(max: Int): Int {
-		if (max <= 0) throw IllegalArgumentException("max must be positive")
-
-		var x = x.get()
 		x = x xor (x shr 12)
 		x = x xor (x shl 25)
 		x = x xor (x shr 27)
 		x *= 2685821657736338717L
-		this.x.set(x)
 
 		val factor = Math.abs(x) / Long.MAX_VALUE.toDouble()
 		return (max * factor).toInt()
