f39f0bc43582f320e98ff689cd7fadfc6207c996
Thomas Nappo <technology@live.com>
2016-05-04 04:53:11 -0400

    Added type-inferenced writing support to `Packeteer` via add operator

--- src/main/kotlin/com/acelta/packet/Packeteer.kt
+++ src/main/kotlin/com/acelta/packet/Packeteer.kt
@@ -1,5 +1,6 @@
 package com.acelta.packet
 
+import com.acelta.util.StringCache
 import com.acelta.util.delegator
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.DefaultByteBufHolder
@@ -20,18 +21,32 @@ class Packeteer(data: ByteBuf) : DefaultByteBufHolder(data) {
 
 	val boolean by delegator<Packeteer, Boolean> { data.readBoolean() }
 
-	private val sb = StringBuilder()
+	private val chars = CharArray(256)
 
 	val string by delegator<Packeteer, String> {
-		sb.delete(0, sb.length - 1)
+		var index = 0
 		while (data.readableBytes() > 0) {
-			val char = data.readUnsignedByte()
-			if ('\n'.toShort() == char) break
-			sb.append(char)
+			val char = data.readUnsignedByte().toChar()
+			if ('\n' == char) break
+			chars[index++] = char
 		}
-		sb.toString() // TODO find way to make string without garbage
+		StringCache[chars, index]
 	}
 
-	inline operator fun <reified T : Any> plus(value: T) = apply { TODO() }
+	inline operator fun <reified T : Any> plus(value: T) = apply {
+		with (content()) {
+			val type = T::class.java
+			when (type) {
+				java.lang.Byte::class.java -> writeByte(value as Int)
+				java.lang.Short::class.java -> writeShort(value as Int)
+				java.lang.Integer::class.java -> writeInt(value as Int)
+				java.lang.Long::class.java -> writeLong(value as Long)
+				java.lang.Float::class.java -> writeFloat(value as Float)
+				java.lang.Double::class.java -> writeDouble(value as Double)
+				java.lang.Boolean::class.java -> writeBoolean(value as Boolean)
+				else -> throw IllegalArgumentException("Could not write the reified type \"$type\"")
+			}
+		}
+	}
 
 }
