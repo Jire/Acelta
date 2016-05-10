1dc88f48c3207864306df37701436a3165dd5f3f
Thomas Nappo <technology@live.com>
2016-05-10 02:01:49 -0400

    Improved `PacketDispatcher` by adding lambda val for automatic with

--- src/main/kotlin/com/acelta/packet/incoming/PacketDispatcher.kt
+++ src/main/kotlin/com/acelta/packet/incoming/PacketDispatcher.kt
@@ -3,14 +3,12 @@ package com.acelta.packet.incoming
 import com.acelta.packet.Packeteer
 import it.unimi.dsi.fastutil.objects.ObjectArrayList
 
-abstract class PacketDispatcher<T : PacketListener> {
+abstract class PacketDispatcher<T : PacketListener>(val receive: Packeteer.(PacketDispatcher<T>) -> Any) {
 
 	protected /* visible for inline */ val listeners = ObjectArrayList<T>()
 
 	fun attach(listener: T) = listeners.add(listener)
 
 	inline fun dispatch(body: T.() -> Any) = listeners.forEach { it.body() }
-
-	abstract fun receive(data: Packeteer): Any
 
 }
