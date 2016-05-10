37f032dfcaf1c70809952889f083d47d41cddae7
Thomas Nappo <technology@live.com>
2016-05-10 09:54:17 -0400

    Epoll is now used if supported on the platform.

--- src/main/kotlin/com/acelta/net/Server.kt
+++ src/main/kotlin/com/acelta/net/Server.kt
@@ -1,6 +1,10 @@
 package com.acelta.net
 
 import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ServerChannel
+import io.netty.channel.epoll.Epoll
+import io.netty.channel.epoll.EpollEventLoopGroup
+import io.netty.channel.epoll.EpollServerSocketChannel
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import java.net.InetSocketAddress
@@ -9,9 +13,12 @@ object Server {
 
 	const val DEFAULT_PORT = 43594
 
+	val epoll = Epoll.isAvailable()
+
 	private val bootstrap = ServerBootstrap()
-	private val group = NioEventLoopGroup()
-	private val channel = NioServerSocketChannel::class.java
+	private val group = if (epoll) EpollEventLoopGroup() else NioEventLoopGroup()
+	private val channel = (if (epoll) EpollServerSocketChannel::class.java
+	else NioServerSocketChannel::class.java) as Class<ServerChannel>
 
 	fun bind(port: Int = DEFAULT_PORT) = bind(InetSocketAddress(port))
 
