d72537adef3e179cc6521150eea0ba0d25878ae8
Thomas Nappo <technology@live.com>
2016-05-16 06:37:26 -0400

    Added automatic packet registration for `PacketConductor`s.

--- src/main/kotlin/com/acelta/packet/PacketConductor.kt
+++ src/main/kotlin/com/acelta/packet/PacketConductor.kt
@@ -6,10 +6,19 @@ import com.acelta.packet.incoming.guest.Handshake
 import com.acelta.packet.incoming.guest.Login
 import com.acelta.packet.outgoing.handshakeResponse
 import com.acelta.packet.outgoing.loginResponse
+import org.reflections.Reflections
 
-abstract class PacketConductor(packetCapacity: Int = 256) {
+abstract class PacketConductor(packageExtension: String, packetCapacity: Int = 256) {
 
 	private val incoming = arrayOfNulls<Packet<*>>(packetCapacity)
+
+	init {
+		val ref = Reflections("${javaClass.`package`.name}.$packageExtension")
+		for (packet in ref.getSubTypesOf(Packet::class.java)) {
+			val obj = packet.kotlin.objectInstance!!
+			this[obj.id] = obj
+		}
+	}
 
 	operator fun set(id: Int, packet: Packet<*>) {
 		incoming[id] = packet
@@ -20,18 +29,14 @@ abstract class PacketConductor(packetCapacity: Int = 256) {
 		packet(input, session)
 	}
 
-	object Guest : PacketConductor() {
+	object Guest : PacketConductor("incoming.guest") {
 		init {
-			// Manually register packets
-			this[Handshake.id] = Handshake
-			this[Login.id] = Login
-
-			// Simplistic packet handlers for login
+			// Simplistic packet handlers for login, should be done in a plugin.
 			Handshake { nameHash -> handshakeResponse(2, 0).flush() }
 			Login { ver, rel, hd, uid, user, pass -> loginResponse(2, 2, false).flush() }
 		}
 	}
 
-	object Game : PacketConductor()
+	object Game : PacketConductor("incoming.game")
 
 }
