1df12b549ae6424e89c258be8e7421a8a0f4d08f
Thomas Nappo <technology@live.com>
2016-05-16 06:25:48 -0400

    Moved manual packet registration to `PacketConductor.Guest`'s initializer. Added simple packet handling to showcase how packets might be handled for login.

--- src/main/kotlin/com/acelta/packet/PacketConductor.kt
+++ src/main/kotlin/com/acelta/packet/PacketConductor.kt
@@ -4,22 +4,30 @@ import com.acelta.net.Session
 import com.acelta.packet.incoming.Packet
 import com.acelta.packet.incoming.guest.Handshake
 import com.acelta.packet.incoming.guest.Login
+import com.acelta.packet.outgoing.handshakeResponse
+import com.acelta.packet.outgoing.loginResponse
 
 abstract class PacketConductor(packetCapacity: Int = 256) {
 
-	private val incoming = arrayOfNulls<Packet<*>>(packetCapacity)
-
-	init {
-		incoming[Handshake.id] = Handshake
-		incoming[Login.id] = Login
-	}
+	protected val incoming = arrayOfNulls<Packet<*>>(packetCapacity)
 
 	fun incoming(id: Int, session: Session, input: Packeteer) {
 		val packet = incoming[id] ?: return
 		packet(input, session)
 	}
 
-	object Guest : PacketConductor()
+	object Guest : PacketConductor() {
+		init {
+			// Manually register packets
+			incoming[Handshake.id] = Handshake
+			incoming[Login.id] = Login
+
+			// Simplistic packet handlers for login
+			Handshake { nameHash -> handshakeResponse(2, 0).flush() }
+			Login { ver, rel, hd, uid, user, pass -> loginResponse(2, 2, false).flush() }
+		}
+	}
+	
 	object Game : PacketConductor()
 
 }
