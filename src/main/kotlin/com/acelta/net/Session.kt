c0f74be89f847cc1c5183eae0296e96a8ba8e079
Thomas Nappo <technology@live.com>
2016-05-17 07:50:53 -0400

    Fixed major `Session` bug with flushing

--- src/main/kotlin/com/acelta/net/Session.kt
+++ src/main/kotlin/com/acelta/net/Session.kt
@@ -4,7 +4,6 @@ import com.acelta.game.Player
 import com.acelta.packet.ByteBufPacketeer
 import com.acelta.packet.PacketConductor
 import com.acelta.packet.SplitPacketeer
-import com.acelta.util.log
 import io.netty.buffer.Unpooled
 import io.netty.channel.Channel
 import java.util.concurrent.atomic.AtomicReference
@@ -15,15 +14,13 @@ class Session(val channel: Channel, override var write: ByteBufPacketeer = ByteB
 	val conductor: AtomicReference<PacketConductor> = AtomicReference(PacketConductor.Guest)
 	lateinit var player: Player
 
-	fun flush() {
-		write.retain()
-		channel.writeAndFlush(write)
-		write.content().clear()
+	fun flush() = with(write.content()) {
+		retain()
+		channel.writeAndFlush(this)
+		clear()
 	}
 
 	fun disconnect() {
-		log("DISCONNECTED?")
-
 		channel.close()
 
 		write.release()
